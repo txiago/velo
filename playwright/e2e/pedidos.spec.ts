a286120c5568ca2548c5f3dfb5f1c72bf44d368c
Thiago Aires <tximedeiros@gmail.com>
2026-02-09 20:27:35 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,17 +1,20 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
+
 test('test', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/');
-  //checkpoint 1: verificar se o título da pagina esta correto
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  //checkpoint 2: verificar se a pagina de consulta de pedidos esta correta
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-U903B1');
   await page.getByTestId('search-order-button').click();
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-U903B1');
 
